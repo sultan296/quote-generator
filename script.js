a6b17e87ad67e5a7984e7f95e76cf3f4edc9c803
Shaik Tippu Sultan <shaiktippu296@gmail.com>
2025-05-05 23:43:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const apiURL = 'https://qapi.vercel.app/api/random';
+        let quote = document.getElementById("quote");
+        let writer = document.getElementById("writer");
+
+        async function quoteCreate() {
+        let response  = await fetch(apiURL);
+            let data = await response.json();
+            quote.innerHTML = data.quote;
+            writer.innerHTML = "-- "+ data.author;
+        }
+        
