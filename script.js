57276c6db560bf296c5b20d6444a7365fd6c4b02
Shaik Tippu Sultan <shaiktippu296@gmail.com>
2025-06-15 12:57:30 +0530

    Update script.js
    Changed API url

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const apiURL = 'https://qapi.vercel.app/api/random';
+const apiURL = 'https://dummyjson.com/quotes/random';
         let quote = document.getElementById("quote");
         let writer = document.getElementById("writer");
 
         async function quoteCreate() {
-        let response  = await fetch(apiURL);
+            let response  = await fetch(apiURL);
             let data = await response.json();
             quote.innerHTML = data.quote;
             writer.innerHTML = "-- "+ data.author;
         }
-        
